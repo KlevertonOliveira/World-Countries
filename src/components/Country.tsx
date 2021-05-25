d87dc8a84839df89711918f7785566709828e3f1
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2021-05-25 18:54:25 -0300

    All done, except create footer!

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export interface ICountry {
 	id: string;
@@ -25,24 +27,55 @@ interface CountryProps {
 
 const Country = ({ country }: CountryProps) => {
 
+	const [overSized, setOverSized] = useState(false);
+	const [showFullContent, setShowFullContent] = useState(false);
+
+	const handleCountryName = (name: string) =>{
+		if(name.length > 13) {
+			setOverSized(true);
+		}
+	}
+
+	useEffect(() => {
+		handleCountryName(country.name);
+	}, [])
+
 	return (
-		<article className='bg-whiteMain shadow-lg w-10/12 sm:w-full rounded-lg mx-auto dark:bg-darkBlue dark:text-whiteMain'>
-			<section
-				className='shadow-md'
-				style={{ height: '176px', width: '100%', position: 'relative' }}
-			>
-				<Image
-					src={country.flag}
-					alt={country.name}
-					layout='fill'
-					objectFit='cover'
-					priority
-					className='rounded-t-md'
-				/>
+		<article className='bg-whiteMain shadow-lg w-10/12 sm:w-full rounded-lg mx-auto dark:bg-darkBlue dark:text-whiteMain border-gray-300 border-2 dark:border-gray-800'>
+			<section className='shadow-md'>
+				<Link href={`/countries/${country.id}`}>
+					<div className='min-h-full h-44 relative hover:opacity-80 transition-opacity duration-300'>
+						<Image
+							src={country.flag}
+							alt={country.name}
+							layout='fill'
+							objectFit='cover'
+							className=' rounded-t-md cursor-pointer'
+						/>
+					</div>
+				</Link>
 			</section>
 
-			<section className='py-6 px-5 mt-2'>
-				<h2 className='text-2xl font-extrabold mb-6'>{country.name}</h2>
+			<section className='p-5 mt-2'>
+				<h2 className='text-2xl font-extrabold mb-6'>
+					{overSized?
+						 
+						 showFullContent?
+							 (`${country.name} `)
+							: (`${country.name.substring(0, 13)}... `)
+						
+							: (country.name)
+					}
+
+					{overSized && (
+						<button
+							className='text-base ml-1 hover:opacity-60' style={{outline:'none', border:'none'}}
+							onClick={() => setShowFullContent(!showFullContent)}
+						>
+							{showFullContent ? '(show less)' : ' (read more)'}
+						</button>
+					)}
+				</h2>
 				<p className='text-lg font-semibold mb-2'>
 					Population:{' '}
 					<span className='font-light'>
